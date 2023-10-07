e016468ad1a6ea6643a4f37db8c8adf2247b6852
Jose Irola <Joseangel0817@gmail.com>
2023-10-07 02:09:35 -0600

    Modulos Base

---
+++ js/CargarPaginaVerUsuario.js
@@ -0,0 +1,4 @@
+function cargarPaginaPHP() {
+    const iframeContainer = document.querySelector('.container');
+    iframeContainer.innerHTML = '<iframe src="ver_usuarios.php" width="100%" height="100%"></iframe>';
+}
