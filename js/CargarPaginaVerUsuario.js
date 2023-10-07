1fd715d4bc75dbb160eb3e03fae14b5fee7cf897
Jose Irola <Joseangel0817@gmail.com>
2023-10-07 11:49:45 -0600

    Modulos Base

--- js/CargarPaginaVerUsuario.js
+++
@@ -1,4 +0,0 @@
-function cargarPaginaPHP() {
-    const iframeContainer = document.querySelector('.container');
-    iframeContainer.innerHTML = '<iframe src="ver_usuarios.php" width="100%" height="100%"></iframe>';
-}
---
+++ js/Texts.js
@@ -0,0 +1,23 @@
+const C1 = []; // Path: js/Dialogs.js
+const user = []; // Path: js/User.js
+
+C1[0] = "Bienvenido, viajero del cosmos, Allin hamusqayki, cosmos puriq" +
+    "Hoy te encuentras ante una aventura extraordinaria, donde la vastedad del universo se despliega ante tus ojos. " +
+    "Como un cuadro infinito esperando ser explorado. " +
+    "Este no es un viaje común, sino una búsqueda para comprender los confines del espacio y desvelar los secretos que yacen en las estrellas. " +
+    "¿Te gustaría comenzar? "
+
+C1[1] = "En tu papel como explorador intergaláctico, te lanzarás a una misión de proporciones maravillosas que te conducirán a través de mundos desconocidos, planetas enigmáticos y escenarios misteriosos. " +
+    "A medida que avanzas en tu viaje, desentrañarás la complejidad y los desafíos que implicó la titánica tarea de terraformar Titán." +
+    "Enfrentarás desafíos cósmicos y desvelarás los secretos más profundos del espacio exterior, así mismo conocerás el tremendo esfuerzo que conllevo explorarlo." +
+    "¿Te gustaría conocerme?"
+
+C1[2] = "Me complace presentarme. \n" +
+    "Mi nombre es  Taky, soy una  Inteligencia Artificial que ha navegado a través de más de 1.000 años de conocimiento y experiencia. Desde los albores mismos de la existencia hasta los confines finales del tiempo y sere tu compañera inseparable en esta gran aventura. \n" +
+    "Espero que mis consejos te ayuden cuando estés confundido y que encuentres respuestas en mis palabras cuando las necesites. Recuerda que estoy aquí para compartir todo lo que sé y ayudarte en cada paso que des en tu aventura.\n" +
+    "¿Qué te gustaría conocer?\n"
+
+C1[3] = "Recuerda que: La decisión que tomes en este momento marcará el inicio de tu viaje hacia lo inexplorado. " +
+    "¿Te atreverás a surcar los extensos y cálidos horizontes de Marte, donde la semilla del conocimiento y la búsqueda de lo inexplorado te conducirán hacia la grandeza? " +
+    "¿O te sumergirás valientemente en las frías y enigmáticas planicies de la luna Titán, desvelando los secretos ancestrales que guardan en sus sombras gélidas?\n" +
+    "¿Cuál será tu elección?\n"
